e259973b7594dcb4f49270a20d258b31e99939f2
jvega190 <jose.vega@craftersoftware.com>
2020-11-06 12:17:01 -0600

    Add/update build script

--- sources/angular-app/build.sh
+++ sources/angular-app/build.sh
@@ -7,21 +7,21 @@ echo "Making sure all deps are met"
 # echo "Compiling relay stuff"
 # yarn relay
 
-echo "Running react build"
-react-scripts build
-
 echo "Deleting prior build"
 rm -rf ../../static-assets/app/*
 
 echo "Restoring .gitkeep"
 touch ../../static-assets/app/.gitkeep
 
-echo "Copying app build to static assets"
-cp -rf ./build/* ../../static-assets/app/
+echo "Running angular build"
+ng build
+
+#echo "Copying app build to static assets"
+#cp -rf ./dist/* ../../static-assets/app/
 
 git add ../../static-assets/app/
 
-echo "Delete react build output directory"
-rm -rf ./build
+#echo "Delete angular build output directory"
+#rm -rf ./dist
 
 echo "Build finished"
